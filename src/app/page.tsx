cca1111516c8938cd90b5e9b335cdd9646c370d6
zdim <zdim227@gmail.com>
2023-12-30 16:01:06 -0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<h1>bean log</h1>
+			<div className="flex justify-end">
+				<button>Add Bean</button>
+			</div>
+			<div>
+
+			</div>
+		</main>
+	)
+}
